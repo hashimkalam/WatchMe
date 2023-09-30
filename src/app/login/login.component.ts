573177e1abf39eb81a05efe677cd84a7432e1747
hashimkalam <hashim.20211291@iit.ac.lk>
2023-09-30 12:28:29 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 
@@ -7,31 +7,46 @@ import { Router } from '@angular/router';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   username = '';
   password = '';
   errorMsg = '';
+  unameFeedbackMsg = '';
 
   constructor(private auth: AuthService, private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // It's good practice to put initialization logic in ngOnInit
+  }
 
   login() {
+    this.errorMsg = '';
     if (this.username.trim().length === 0) {
       this.errorMsg = 'Username is required';
-    } else if (this.password.trim().length === 0) {
+      return;
+    }
+
+    if (this.password.trim().length === 0) {
       this.errorMsg = 'Password is required';
+      return;
+    }
+
+    if (this.username.trim().length < 5) {
+      this.unameFeedbackMsg = 'Username must contain at least 5 characters';
+      return;
+    } else if (this.username.trim().length > 16) {
+      this.unameFeedbackMsg = 'Username must contain less than 17 characters';
+      return;
     } else {
-      this.errorMsg = '';
-      let res = this.auth.login(this.username, this.password);
+      this.unameFeedbackMsg = '';
+    }
 
-      if (res === 200) {
-        this.router.navigate(['home']);
-      }
+    const res = this.auth.login(this.username, this.password);
 
-      if (res === 403) {
-        this.router.navigate(['Invalid Credentials']);
-      }
+    if (res === 200) {
+      this.router.navigate(['home']);
+    } else if (res === 403) {
+      this.errorMsg = 'Invalid Credentials';
     }
   }
 }
